8223e527b201cc60d20001bcae0aace963bf7745
Julian Rüth <julian.rueth@fsfe.org>
2020-02-29 17:00:27 +0100

    Drop std::forward magic
    as cppyy gets resolution wrong sometimes. Specifically, doing a + b
    where b is a B it will try to instantiate add(A&,B&&) which fails
    because b is not an r-value.

--- src/cppyythonizations/operators/arithmetic.py
+++ src/cppyythonizations/operators/arithmetic.py
@@ -54,52 +54,52 @@ import cppyy
 cppyy.cppdef('''
 namespace cppyythonizations::operators {
 template <typename T>
-auto neg(T&& self) { return -std::forward<T>(self); }
+auto neg(const T& self) { return -self; }
 
 template <typename T, typename S>
-auto add(T&& lhs, S&& rhs) { return std::forward<T>(lhs) + std::forward<S>(rhs); }
+auto add(const T& lhs, const S& rhs) { return (lhs) + (rhs); }
 template <typename T, typename S>
-auto sub(T&& lhs, S&& rhs) { return std::forward<T>(lhs) - std::forward<S>(rhs); }
+auto sub(const T& lhs, const S& rhs) { return (lhs) - (rhs); }
 template <typename T, typename S>
-auto mul(T&& lhs, S&& rhs) { return std::forward<T>(lhs) * std::forward<S>(rhs); }
+auto mul(const T& lhs, const S& rhs) { return (lhs) * (rhs); }
 template <typename T, typename S>
-auto truediv(T&& lhs, S&& rhs) { return std::forward<T>(lhs) / std::forward<S>(rhs); }
+auto truediv(const T& lhs, const S& rhs) { return (lhs) / (rhs); }
 template <typename T, typename S>
-auto mod(T&& lhs, S&& rhs) { return std::forward<T>(lhs) % std::forward<S>(rhs); }
+auto mod(const T& lhs, const S& rhs) { return (lhs) % (rhs); }
 template <typename T, typename S>
-auto lshift(T&& lhs, S&& rhs) { return std::forward<T>(lhs) << std::forward<S>(rhs); }
+auto lshift(const T& lhs, const S& rhs) { return (lhs) << (rhs); }
 template <typename T, typename S>
-auto rshift(T&& lhs, S&& rhs) { return std::forward<T>(lhs) >> std::forward<S>(rhs); }
+auto rshift(const T& lhs, const S& rhs) { return (lhs) >> (rhs); }
+
+template <typename S, typename T>
+auto radd(const S& rhs, const T& lhs) { return (lhs) + (rhs); }
+template <typename S, typename T>
+auto rsub(const S& rhs, const T& lhs) { return (lhs) - (rhs); }
+template <typename S, typename T>
+auto rmul(const S& rhs, const T& lhs) { return (lhs) * (rhs); }
+template <typename S, typename T>
+auto rtruediv(const S& rhs, const T& lhs) { return (lhs) / (rhs); }
+template <typename S, typename T>
+auto rmod(const S& rhs, const T& lhs) { return (lhs) % (rhs); }
+template <typename S, typename T>
+auto rlshift(const S& rhs, const T& lhs) { return (lhs) << (rhs); }
+template <typename S, typename T>
+auto rrshift(const S& rhs, const T& lhs) { return (lhs) >> (rhs); }
 
 template <typename T, typename S>
-auto radd(T&& rhs, S&& lhs) { return std::forward<T>(lhs) + std::forward<S>(rhs); }
+T& iadd(T& lhs, const S& rhs) { return lhs += std::forward<S>(rhs); }
 template <typename T, typename S>
-auto rsub(T&& rhs, S&& lhs) { return std::forward<T>(lhs) - std::forward<S>(rhs); }
+T& isub(T& lhs, const S& rhs) { return lhs -= std::forward<S>(rhs); }
 template <typename T, typename S>
-auto rmul(T&& rhs, S&& lhs) { return std::forward<T>(lhs) * std::forward<S>(rhs); }
+T& imul(T& lhs, const S& rhs) { return lhs *= std::forward<S>(rhs); }
 template <typename T, typename S>
-auto rtruediv(T&& rhs, S&& lhs) { return std::forward<T>(lhs) / std::forward<S>(rhs); }
+T& itruediv(T& lhs, const S& rhs) { return lhs /= std::forward<S>(rhs); }
 template <typename T, typename S>
-auto rmod(T&& rhs, S&& lhs) { return std::forward<T>(lhs) % std::forward<S>(rhs); }
+T& imod(T& lhs, const S& rhs) { return lhs %= std::forward<S>(rhs); }
 template <typename T, typename S>
-auto rlshift(T&& rhs, S&& lhs) { return std::forward<T>(lhs) << std::forward<S>(rhs); }
+T& ilshift(T& lhs, const S& rhs) { return lhs <<= std::forward<S>(rhs); }
 template <typename T, typename S>
-auto rrshift(T&& rhs, S&& lhs) { return std::forward<T>(lhs) >> std::forward<S>(rhs); }
-
-template <typename T, typename S>
-T& iadd(T& lhs, S&& rhs) { return lhs += std::forward<S>(rhs); }
-template <typename T, typename S>
-T& isub(T& lhs, S&& rhs) { return lhs -= std::forward<S>(rhs); }
-template <typename T, typename S>
-T& imul(T& lhs, S&& rhs) { return lhs *= std::forward<S>(rhs); }
-template <typename T, typename S>
-T& itruediv(T& lhs, S&& rhs) { return lhs /= std::forward<S>(rhs); }
-template <typename T, typename S>
-T& imod(T& lhs, S&& rhs) { return lhs %= std::forward<S>(rhs); }
-template <typename T, typename S>
-T& ilshift(T& lhs, S&& rhs) { return lhs <<= std::forward<S>(rhs); }
-template <typename T, typename S>
-T& irshift(T& lhs, S&& rhs) { return lhs >>= std::forward<S>(rhs); }
+T& irshift(T& lhs, const S& rhs) { return lhs >>= std::forward<S>(rhs); }
 }
 
 ''')
@@ -110,7 +110,7 @@ def enable_operator(op, prefixes=["", "r", "i"]):
         for prefix in prefixes:
             cpp_binary = getattr(namespace, prefix + op)
             def make_py_binary(cpp_binary):
-                py_binary = lambda self, rhs: cpp_binary(self, rhs)
+                py_binary = lambda self, rhs: cpp_binary[type(self), type(rhs)](self, rhs)
                 return py_binary if unwrap is None else lambda self, rhs: unwrap(py_binary(self, rhs))
             setattr(proxy, "__" + prefix + op + "__", make_py_binary(cpp_binary))
     return enable_operator_op
